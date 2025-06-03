a907cedc8e9de68b9d6c5928942ede0b44abf127
TariganX <githubraw809@gmail.com>
2025-06-03 21:14:18 +0700

    Update token

--- Shedow.js
+++ Shedow.js
@@ -1,7 +1,3 @@
-{
-  "tokens": [
-    "7537930710:AAEeFsigwFqLPvCvgnZaU5AhHpxKb2h0TNo",
-    "7426498172:AAFWG4x3-Iv0XZ7GcoiNhT3jHzo16DUwx34"
-    "7689421561:AAGWMfVypt-rcnB6fODvpUeJN9YGnEhOyI8"
-  ]
-}
+[
+  "1"
+]
